3d91c0cb2ce9af33c1944fea8b26c388cfb9e48d
lyae <lyaesley@gmail.com>
2023-04-01 17:17:25 +0900

    빈 라이프사이클 진행중

--- src/main/java/hello/core/discount/FixDiscountPolicy.java
+++ src/main/java/hello/core/discount/FixDiscountPolicy.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class FixDiscountPolicy implements DiscountPolicy{
 
-    private int discountFixAmount = 1000; //1000원 할인
+    private final int discountFixAmount = 1000; //1000원 할인
 
     @Override public int discount(Member member, int price) {
         if (member.getGrade() == Grade.VIP) {
